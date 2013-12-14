1cdb16c6553dfc556c21aa1e34ac2fe97ee03c45
webber <web-ber12@yandex.ru>
2013-12-14 16:38:10 +0200

    add &config param for config form output

--- assets/modules/functions.easyForm.php
+++ assets/modules/functions.easyForm.php
@@ -1,6 +1,11 @@
 <?php
 if(!defined('MODX_BASE_PATH')){die('What are you doing? Get out of here!');}
 
+function eParseTpl($arr1,$arr2,$output){
+	return str_replace($arr1,$arr2,$output);
+}
+
+
 function getFormInfo($id){
 	global $modx;
 	$info=array();
@@ -10,13 +15,25 @@ function getFormInfo($id){
 	return $info;
 }
 
-function makeTpl($id,$capcha=false){
+function makeTpl($id,$capcha=false,$config='default'){
 	global $modx;
-	$f='';
+
+	$outer='';
+	$fields='';
+	
 	if(isset($id)&&$modx->db->getRecordCount($modx->db->query("SELECT * FROM ".$modx->getFullTableName('forms')." WHERE id=".$id." LIMIT 0,1"))==1)
 	{
+		//подключаем файл конфигурации с шаблонами
+		if(is_file(MODX_BASE_PATH.'assets/modules/easyForm/config/config.'.$config.'.php')){
+			include_once(MODX_BASE_PATH.'assets/modules/easyForm/config/config.'.$config.'.php');
+		}
+		else{
+			include_once(MODX_BASE_PATH.'assets/modules/easyForm/config/config.default.php');
+		}
+		
 		$forma=$modx->db->query("SELECT * FROM ".$modx->getFullTableName('form_fields')." WHERE parent=".$id." ORDER BY sort ASC");
 		$forma_info=$modx->db->getRow($modx->db->query("SELECT * FROM ".$modx->getFullTableName('forms')." WHERE id=".$id." LIMIT 0,1"));
+		
 		if($modx->db->getRecordCount($forma)>0){
 			$form=array();
 			while($row=$modx->db->getRow($forma)){
@@ -25,26 +42,19 @@ function makeTpl($id,$capcha=false){
 				$form[$row['id']]['value']=$row['value'];
 				$form[$row['id']]['required']=$row['required'];
 			}
-
-			$f='
-				<div class="f_zagol">'.$forma_info['name'].'</div>
-				<div class="f_description">'.$forma_info['title'].'</div>
-				<div class="f_form f_form'.$id.'" id="f_form'.$id.'">
-					<div class="validation_message">[+validationmessage+]</div>
-					<form id="f'.$id.'" class="easyForm" action="[~[*id*]~]" method="post">		
-			';
+			
+			$fields='';
+			
 			foreach($form as $k=>$v){
 				$req=$v['required']==1?1:0;
 				$type=$v['type']==3?'email':'string';
-				$f.='<div class="f_row f_row'.$k.'"><div class="f_title">'.$v['title'].' '.($req==1?'<span class="red">*</span>':'').'</div><div class="field">';
 				
 				switch($v['type']){
 				  case 2:
-					$f.="<textarea name='param".$k."' class='f_txtarea' eform='".$v['title'].":".$type.":".$req."'></textarea>";
+					$field="<textarea name='param".$k."' class='f_txtarea' eform='".$v['title'].":".$type.":".$req."'></textarea>";
 					break;
 				
 				  case 5:
-					$f.="<div class='selector'><select name='param".$k."' class='f_selector' eform='".$v['title']."::".$req."'>";
 					$opts=explode("\n",$v['value']);
 					$opt='';
 					foreach($opts as $k1=>$v1){
@@ -54,11 +64,10 @@ function makeTpl($id,$capcha=false){
 						$val=isset($arr[1])&&$arr[1]!=''?$arr[1]:$arr[0];
 						$opt.="<option value='".$key."'>".$val."</option>";
 					}
-					$f.=$opt."</select></div>";
+					$field="<div class='selector'><select name='param".$k."' class='f_selector' eform='".$v['title']."::".$req."'>".$opt."</select></div>";
 					break;
 				
 				  case 6:
-					$f.="<div class='radio'>";
 					$opts=explode("\n",$v['value']);
 					$opt='';
 					foreach($opts as $k1=>$v1){
@@ -68,11 +77,10 @@ function makeTpl($id,$capcha=false){
 						$val=isset($arr[1])&&$arr[1]!=''?$arr[1]:$arr[0];
 						$opt.="<label>".$val." <input type='radio' name='param".$k."' value='".$key."' ".($k1==0?"eform='".$v['title']."::".$req."'":"")."></label>";
 					}
-					$f.=$opt."</div>";
+					$field="<div class='radio'>".$opt."</div>";
 					break;
 					
 				  case 7:
-					$f.="<div class='checkbox'>";
 					$opts=explode("\n",$v['value']);
 					$opt='';
 					foreach($opts as $k1=>$v1){
@@ -82,15 +90,14 @@ function makeTpl($id,$capcha=false){
 						$val=isset($arr[1])&&$arr[1]!=''?$arr[1]:$arr[0];
 						$opt.="<label>".$val." <input type='checkbox' name='param".$k."[]' value='".$key."' ".($k1==0?"eform='".$v['title']."::".$req."'":"")."></label>";
 					}
-					$f.=$opt."</div>";
+					$field="<div class='checkbox'>".$opt."</div>";
 					break;
 					
 				  case 8:
-					$f.="<input type='file' name='param".$k."' class='f_file' eform='".$v['title'].":".$type.":".$req."'>";
+					$field="<input type='file' name='param".$k."' class='f_file' eform='".$v['title'].":".$type.":".$req."'>";
 					break;
 					
 				  case 9:
-					$f.="<div class='multiselector'><select name='param".$k."[]' class='f_multiselector' multiple='multiple' size='5' eform='".$v['title']."::".$req."'>";
 					$opts=explode("\n",$v['value']);
 					$opt='';
 					foreach($opts as $k1=>$v1){
@@ -100,32 +107,40 @@ function makeTpl($id,$capcha=false){
 						$val=isset($arr[1])&&$arr[1]!=''?$arr[1]:$arr[0];
 						$opt.="<option value='".$key."'>".$val."</option>";
 					}
-					$f.=$opt."</select></div>";
+					$field="<div class='multiselector'><select name='param".$k."[]' class='f_multiselector' multiple='multiple' size='5' eform='".$v['title']."::".$req."'>".$opt."</select></div>";
 					break;
 				
 				  case 10:
-					$f.="<input type='hidden' name='param".$k."' value='' class='f_hidden' eform='".$v['title'].":".$type.":".$req."'>";
+					$field="<input type='hidden' name='param".$k."' value='' class='f_hidden' eform='".$v['title'].":".$type.":".$req."'>";
 					break;
 					
 				  default:
-					$f.="<input type='text' name='param".$k."' value='' class='f_txt' eform='".$v['title'].":".$type.":".$req."'>";
+					$field="<input type='text' name='param".$k."' value='' class='f_txt' eform='".$v['title'].":".$type.":".$req."'>";
 					break;
 				}
-			
-				$f.='</div></div>';
+	
+				$req_text=($req==1?'<span class="red">*</span>':'');
+				$fields.=eParseTpl(
+					array('[+num+]','[+title+]','[+req_text+]','[+field+]'),
+					array($k,$v['title'],$req_text,$field),
+					$rowTpl
+				);
 			}
 			if($capcha){
-				$f.='
-				<div class="f_capcha">
-					<div class="f_title">Введите код с картинки: </div>
-					<div class="f_field"><input type="text" class="f_ver" name="vericode" /><div class="f_image_capcha"><img class="feed" id="capcha'.$id.'" src="[+verimageurl+]" alt="Введите код" /></div><div class="f_renew_capcha"><a href="javascript:;" onclick="document.getElementById(\'capcha'.$id.'\').src=\''.MODX_BASE_URL.MGR_DIR.'/includes/veriword.php?rand=\'+Math.random();">обновить картинку</a></div></div>
-				</div>
-				';
+				$fields.=eParseTpl(
+					array('[+id+]','[+capcha_dir+]'),
+					array($id,MODX_BASE_URL.MGR_DIR),
+					$capchaTpl
+				);
 			}
-			$f.='<div class="f_sendbutton"><input type="submit" value="Отправить"</div>';
-			$f.='</form></div>';
+		
+			$outer=eParseTpl(
+				array('[+form_name+]','[form_description]','[+id+]','[+fields+]'),
+				array($forma_info['name'],$forma_info['title'],$id,$fields),
+				$outerTpl
+			);
 		}
-	return $f;
+	return $outer;
 	}
 }
 
